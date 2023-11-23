0ac0ceaf4285a05141f0c4e2d0eb833948535a3d
Munawar <134474387+munawar3206@users.noreply.github.com>
2023-11-23 17:30:30 +0530

    commit on 23-11-eve

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chattogether/apis/api.dart';
 import 'package:chattogether/main.dart';
 import 'package:chattogether/model/model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -28,30 +29,75 @@ class _ProfileScreenState extends State<ProfileScreen> {
         elevation: 4,
         backgroundColor: Colors.black,
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () async {
-          await FirebaseAuth.instance.signOut();
-          await GoogleSignIn().signOut();
-        },
-        child: const Icon(
-          Icons.add_comment_rounded,
-        ),
-      ),
-      body: Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(mq.height * .03),
-          child: CachedNetworkImage(
-            width: mq.height * .055,
-            height: mq.height * .055,
-            imageUrl: widget.user.image,
-            // placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const CircleAvatar(
-              child: Icon(CupertinoIcons.person),
-            ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton.extended(
+          label: Text("signout"),
+          backgroundColor: const Color.fromARGB(255, 245, 9, 9),
+          onPressed: () async {
+            await Apis.auth.signOut();
+            await GoogleSignIn().signOut();
+          },
+          icon: Icon(
+            Icons.logout_outlined,
           ),
         ),
-      ]),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: mq.width * .05),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              width: mq.width,
+              height: mq.height * .1,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(mq.height * .1),
+              child: CachedNetworkImage(
+                width: mq.height * .2,
+                height: mq.height * .2,
+                imageUrl: widget.user.image,
+                fit: BoxFit.fill,
+                // placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const CircleAvatar(
+                  child: Icon(CupertinoIcons.person),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: mq.height * .03,
+            ),
+            Text(widget.user.email),
+            SizedBox(
+              height: mq.height * .03,
+            ),
+            TextFormField(
+              initialValue: widget.user.name,
+              decoration: InputDecoration(
+                hintText: "eg. Avarankutty",
+                label: const Text("Name"),
+                prefixIcon: const Icon(Icons.person),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+            SizedBox(
+              height: mq.height * .03,
+            ),
+            TextFormField(
+              initialValue: widget.user.about,
+              decoration: InputDecoration(
+                  hintText: "eg. ntha barthanam",
+                  label: const Text("About"),
+                  prefixIcon: const Icon(Icons.info_outline),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+            ElevatedButton.icon(
+                onPressed: () {}, icon: Icon(Icons.edit), label: Text("UPDATE"),)
+          ]),
+        ),
+      ),
     );
   }
 }
