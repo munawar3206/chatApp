99fe71bbb675e60c70e23561db03a1ddf5cf1225
Munawar <134474387+munawar3206@users.noreply.github.com>
2023-11-27 15:52:06 +0530

    commits added bottomsheet for crud

--- lib/view/chatscreen/messages.dart/messagecard.dart
+++ lib/view/chatscreen/messages.dart/messagecard.dart
@@ -3,7 +3,7 @@ import 'package:chattogether/main.dart';
 import 'package:chattogether/model/message_model.dart';
 import 'package:chattogether/view/chatscreen/messages.dart/widgets/bluemessage.dart';
 import 'package:chattogether/view/chatscreen/messages.dart/widgets/greenmessage.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:chattogether/view/chatscreen/messages.dart/widgets/option_item.dart';
 import 'package:flutter/material.dart';
 
 class MessageCard extends StatefulWidget {
@@ -19,7 +19,9 @@ class _MessageCardState extends State<MessageCard> {
   Widget build(BuildContext context) {
     bool isMe = Apis.user.uid == widget.message.fromId;
     return InkWell(
-        onLongPress: () {},
+        onLongPress: () {
+          _showbottomsheet(isMe);
+        },
         child: isMe
             ? BlueMessage(
                 widget: widget,
@@ -30,5 +32,90 @@ class _MessageCardState extends State<MessageCard> {
     // : GreenMessage(
     //     widget: widget,
     //   );
+  }
+
+// bottom sheet modify msg
+  void _showbottomsheet(bool isMe) {
+    showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        builder: (_) {
+          return ListView(
+            shrinkWrap: true,
+            children: [
+              Container(
+                height: 4,
+                margin: EdgeInsets.symmetric(
+                    vertical: mq.height * .015, horizontal: mq.width * .4),
+                decoration: const BoxDecoration(color: Colors.grey),
+              ),
+              widget.message.type == Type.text
+                  ? OptionItem(
+                      icon: const Icon(
+                        Icons.copy_all_outlined,
+                        color: Colors.blue,
+                        size: 26,
+                      ),
+                      name: 'Copy Text',
+                      onTap: () {})
+                  : OptionItem(
+                      icon: const Icon(
+                        Icons.download_done_rounded,
+                        color: Colors.blue,
+                        size: 26,
+                      ),
+                      name: 'Save Image',
+                      onTap: () {}),
+              if (isMe)
+                Divider(
+                  color: Colors.black54,
+                  endIndent: mq.width * .04,
+                  indent: mq.width * .04,
+                ),
+              if (widget.message.type == Type.text && isMe)
+                OptionItem(
+                    icon: const Icon(
+                      Icons.edit,
+                      color: Colors.blue,
+                      size: 26,
+                    ),
+                    name: 'Edit Message',
+                    onTap: () {}),
+              if (isMe)
+                OptionItem(
+                    icon: const Icon(
+                      Icons.delete_forever,
+                      color: Color.fromARGB(255, 243, 33, 33),
+                      size: 26,
+                    ),
+                    name: 'Delete Message',
+                    onTap: () {}),
+              Divider(
+                color: Colors.black54,
+                endIndent: mq.width * .04,
+                indent: mq.width * .04,
+              ),
+              OptionItem(
+                  icon: Icon(
+                    Icons.remove_red_eye,
+                    color: Color.fromARGB(255, 255, 251, 0),
+                  ),
+                  name: 'Sent At',
+                  onTap: () {}),
+
+              OptionItem(
+                  icon: Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.green,
+                  ),
+                  name: 'Read At',
+                  onTap: () {}),
+
+             
+            ],
+          );
+        });
   }
 }
